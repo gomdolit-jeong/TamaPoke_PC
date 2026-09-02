4dc5002111d530f5fd2de648abee7b8d75312eee
yoonsang.jeong <yoonsang.jeong89@gmail.com>
2026-09-02 16:18:39 +0900

    - 미사용 코드 삭제

--- Views/PartyView.xaml.cs
+++ Views/PartyView.xaml.cs
@@ -35,39 +35,6 @@ namespace TamaPoke.Views
             }
         }
 
-        // 🌟 액션(배틀 출전/방생) 버튼을 클릭했을 때
-        private void ActionButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.DataContext is PokemonState state)
-            {
-                if (state.IsSwapMode)
-                {
-                    state.CancelSwap();
-                    state.CloseParty();
-                }
-                else
-                {
-                    // 🌟 파티에서 현재 선택된(IsSelected == true) 포켓몬을 찾습니다.
-                    PartyMember? selectedMember = null;
-                    foreach (var member in state.Party)
-                    {
-                        if (member.IsSelected) { selectedMember = member; break; }
-                    }
-
-                    if (selectedMember != null)
-                    {
-                        // TODO: 이 멤버를 출전시키는 배틀 로직을 여기에 구현합니다!
-                        System.Diagnostics.Debug.WriteLine($"{selectedMember.Name} 출전 준비 완료!");
-                        state.CloseParty();
-                    }
-                    else
-                    {
-                        System.Diagnostics.Debug.WriteLine("출전할 포켓몬을 먼저 선택해 주세요.");
-                    }
-                }
-            }
-        }
-
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             if (this.DataContext is PokemonState state)
